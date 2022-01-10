979478e43f35c3b8396478d48c8a8d1e6fdfe95c
TallPanda <b00102169@mytudublin.ie>
2022-01-10 15:32:59 +0000

    More error handling

--- MFD/hashing.py
+++ MFD/hashing.py
@@ -5,19 +5,22 @@
 # @version 1.02 10th of January 2022
 # @platform   Python 3.10.1
 from hashlib import sha1
+import os
+
 
 def chunk(fname: str,gigabytes:int=None):
-    if gigabytes is None:
-        gigabytes=1
-    with open(fname,"rb") as f:
-        while True:
-            curpos = f.tell()
-            fbytes = f.read((2**10)*gigabytes)# this is 1 gb if gibaytes is 1
-            if(curpos == f.tell()): # if position in file hasnt changed break while true loop
-                break
-            else:
-                yield fbytes
-    pass
+    if os.path.isfile(fname):
+        if gigabytes is None:
+            gigabytes=1
+        with open(fname,"rb") as f:
+            while True:
+                curpos = f.tell()
+                fbytes = f.read((2**10)*gigabytes)# this is 1 gb if gibaytes is 1
+                if(curpos == f.tell()): # if position in file hasnt changed break while true loop
+                    break
+                else:
+                    yield fbytes
+
 
 
 def hashing(fname: str,gigabytes:int=None):
